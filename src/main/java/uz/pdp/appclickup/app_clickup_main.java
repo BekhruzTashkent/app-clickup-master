35b064f7d61c2f303e032c51cd8bb74618b3e61c
BekhruzTashkent <bkarimov414@gmail.com>
2022-09-07 09:50:15 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/appclickup/app_clickup_main.java
@@ -0,0 +1,11 @@
+package uz.pdp.appclickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app_clickup_main {
+    public static void main(String[] args) {
+        SpringApplication.run(app_clickup_main.class, args);
+    }
+}
---
+++ src/main/java/uz/pdp/appclickup/entity/enums/SystemRoleName.java
@@ -0,0 +1,9 @@
+package uz.pdp.appclickup.entity.enums;
+
+public enum SystemRoleName {
+
+    SYSTEM_ROLE_ADMIN,
+    SYSTEM_ROLE_MODER,
+    SYSTEM_USER
+
+}
